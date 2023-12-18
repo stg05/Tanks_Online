7fcc7eda486aaf5f8a79050a902feaf8ecc0e323
robodaunill <singit_d@mail.ru>
2023-12-18 20:47:09 +0300

    исправил новые типы кнопок в сценах

--- scenes/online.py
+++ scenes/online.py
@@ -9,12 +9,14 @@ from models import interface_objects as io
 
 def play_online(screen):
     print('playing online')
-    button_exit = io.Button(WIDTH * 0.95, HEIGHT * 0.05, WIDTH * 0.10, HEIGHT * 0.10,
+    button_exit = io.Button(screen, WIDTH * 0.95, HEIGHT * 0.05, WIDTH * 0.10, HEIGHT * 0.10,
                             RED,
                             BLACK, "Exit", io.menu)
 
     buttons = [button_exit]
     while state.scene_type == 'online':
         screen.fill(WHITE)
-        io.check_all_buttons(buttons, screen)
+        io.draw_all_buttons(buttons)
+        for event in pygame.event.get():
+            io.check_all_buttons(event, buttons)
         pygame.display.flip()
