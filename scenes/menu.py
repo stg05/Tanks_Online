86d4710db97eb9b2a3b0a6aeb4b178594db3ec03
robodaunill <singit_d@mail.ru>
2023-12-19 17:44:29 +0300

    сделал переключение фона

--- scenes/menu.py
+++ scenes/menu.py
@@ -14,9 +14,12 @@ def play_menu(screen):
                                 font_dir='fonts/Army.ttf')
 
     buttons = [button_range, button_offline, button_online, button_settings]
+
     # необходим подцикл, т к если создавать кнопки внутри цикла, они будут нажиматься через раз
     while state.scene_type == 'menu':
         screen.fill(WHITE)
+        background_image = io.current_background_image()
+        screen.blit(background_image, (0, 0))
         io.draw_all_buttons(buttons)
         for event in pygame.event.get():
             io.check_all_buttons(event=event, buttons=buttons)
