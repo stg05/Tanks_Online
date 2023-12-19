ee7f54dbae1366ad1d32a5bb50a104bb54c08d78
robodaunill <singit_d@mail.ru>
2023-12-19 19:16:59 +0300

    добавил фоны

--- scenes/background_set.py
+++ scenes/background_set.py
@@ -1,6 +1,6 @@
 import pygame
 
-background1 = pygame.image.load("scenes/scenes_images/test_scene1.png")
-background2 = pygame.image.load("scenes/scenes_images/test_scene2.png")
+background1 = pygame.image.load("scenes/scenes_images/sunny_background.png")
+background2 = pygame.image.load("scenes/scenes_images/volcano_background.png")
 backgrounds = [background1, background2]
 current_background_index = 0
